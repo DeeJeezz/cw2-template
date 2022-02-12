5cf71e1dc0166d1d3338d6f02d03456a66a5a245
Dmitry Shvetsov <killmaster00431@gmail.com>
2022-02-12 20:37:45 +0500

    Починил пути в настройках

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ DATA_STORAGE_NAME: str = 'JSON'
 BASE_DATA_PATH: str = 'data'
 REPO_SETTINGS: Dict[str, str] = {
     'posts_file_path': os.path.join(BASE_DATA_PATH, 'data.json'),
-    'comments_file_path': os.path.join('comments.json'),
-    'bookmarks_file_path': os.path.join('bookmarks.json'),
+    'comments_file_path': os.path.join(BASE_DATA_PATH, 'comments.json'),
+    'bookmarks_file_path': os.path.join(BASE_DATA_PATH, 'bookmarks.json'),
 }
